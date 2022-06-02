09ee0e612817d4eb189a4ee7123597d75fbe6a89
migel777com <62992979+migel777com@users.noreply.github.com>
2022-06-03 00:43:39 +0600

    tt changes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -36,44 +36,47 @@ func (app *application) routes() http.Handler {
 
 	router.GET("/healthcheck", app.Healthcheck)
 
-	router.GET("/group", app.GetAllGroups)
-	router.GET("/teacher", app.GetAllTeachers)
-	router.GET("/room", app.GetAllRooms)
-	router.GET("/room/:roomId", app.GetRoom)
-
-	timetable := router.Group("timetable")
+	tt := router.Group("/tt")
 	{
-		timetable.GET("/group/:group", app.GetGroupTimetable)
-		timetable.GET("/tutor/:tutorId", app.GetTutorTimetable)
-		timetable.GET("/tutor/email/:email", app.GetTutorEmailTimetable)
-		timetable.GET("/room/:roomId", app.GetRoomTimetable)
-	}
+		tt.GET("/group", app.GetAllGroups)
+		tt.GET("/teacher", app.GetAllTeachers)
+		tt.GET("/room", app.GetAllRooms)
+		tt.GET("/room/:roomId", app.GetRoom)
 
-	booking := router.Group("/booking")
-	{
-		booking.GET("", app.GetAllBooking)
-		booking.POST("/datetime", app.GetDateTimeBooking)
-
-		booking.POST("/room/:roomId", app.GetRoomBooking)
-		booking.GET("/reserver/:reserverId", app.GetReserverBooking)
-		booking.POST("/reserver/between/:reserverId", app.GetReserverBetweenBooking)
-
-		booking.GET("/requests", app.GetBookingRequests)
-		booking.GET("/confirm", app.GetConfirmedBooking)
-
-		secured := booking.Group("", app.auth())
+		timetable := tt.Group("/timetable")
 		{
-			secured.POST("/create", app.BookRoom)
-			secured.POST("/confirm/:bookingId", app.ConfirmBooking)
-			secured.POST("/reject/:bookingId", app.RejectBooking)
+			timetable.GET("/group/:group", app.GetGroupTimetable)
+			timetable.GET("/tutor/:tutorId", app.GetTutorTimetable)
+			timetable.GET("/tutor/email/:email", app.GetTutorEmailTimetable)
+			timetable.GET("/room/:roomId", app.GetRoomTimetable)
 		}
-	}
 
-	login := router.Group("/login")
-	{
-		login.POST("/office", app.OfficeAuth)
+		booking := tt.Group("/booking")
+		{
+			booking.GET("", app.GetAllBooking)
+			booking.POST("/datetime", app.GetDateTimeBooking)
+
+			booking.POST("/room/:roomId", app.GetRoomBooking)
+			booking.GET("/reserver/:reserverId", app.GetReserverBooking)
+			booking.POST("/reserver/between/:reserverId", app.GetReserverBetweenBooking)
+
+			booking.GET("/requests", app.GetBookingRequests)
+			booking.GET("/confirm", app.GetConfirmedBooking)
+
+			secured := booking.Group("", app.auth())
+			{
+				secured.POST("/create", app.BookRoom)
+				secured.POST("/confirm/:bookingId", app.ConfirmBooking)
+				secured.POST("/reject/:bookingId", app.RejectBooking)
+			}
+		}
+
+		login := tt.Group("/login")
+		{
+			login.POST("/office", app.OfficeAuth)
+		}
+		tt.GET("/isAdmin/:id", app.auth(), app.isAdmin)
 	}
-	router.GET("/isAdmin/:id", app.auth(), app.isAdmin)
 
 	//users.GET("/userdata", app.auth(), app.UserData)
 
